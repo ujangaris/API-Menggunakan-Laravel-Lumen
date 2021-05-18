2548d5e942907b060bb99ab13cd19f7bb482333e
ujangaris <ujangaja@gmail.com>
2021-05-18 20:18:39 +0700

    Tutorial belajar API menggunakan laravel lumen #6Migration | Pelanggan

--- database/migrations/2021_05_18_131506_create_pelanggans_table.php
+++ database/migrations/2021_05_18_131506_create_pelanggans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateKategorisTable extends Migration
+class CreatePelanggansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateKategorisTable extends Migration
      */
     public function up()
     {
-        Schema::create('kategoris', function (Blueprint $table) {
-            $table->increments('idkategori');
-            $table->string('kategori');
+        Schema::create('pelanggans', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateKategorisTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kategoris');
+        Schema::dropIfExists('pelanggans');
     }
 }
